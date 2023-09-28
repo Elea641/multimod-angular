127a7d7fec22cdf07c6cf25d02c500fcf7e63774
Elea641 <eleavoliotis64@gmail.com>
2023-09-28 17:14:42 +0200

    validator wip

--- src/app/modules/auth/shared/password-validator.ts
+++ src/app/modules/auth/shared/password-validator.ts
@@ -3,17 +3,13 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 export function passwordValidator(control: AbstractControl): ValidationErrors | null {
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
     const valid = passwordRegex.test(control.value);
-  
-    if (!valid) {
-        return { invalidPassword: true };
-    }
-  
+
     const specialCharacterRegex = /[@$!%*?&]/;
     const hasSpecialCharacter = specialCharacterRegex.test(control.value);
 
     const lowercaseCharacterRegex = /[a-z]/;
     const hasLowercaseCharacter = lowercaseCharacterRegex.test(control.value);
-    
+
     const errors: ValidationErrors = {};
 
     if (!hasSpecialCharacter) {
@@ -23,6 +19,10 @@ export function passwordValidator(control: AbstractControl): ValidationErrors | 
     if (!hasLowercaseCharacter) {
         errors['missingLowercaseCharacter'] = true;
     }
+
+    if (!valid) {
+        errors['invalidPassword'] = true
+    };
 
     return Object.keys(errors).length > 0 ? errors : null;
 }
