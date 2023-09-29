00193ab96d463a5e92af23093c1a702329402340
Elea641 <eleavoliotis64@gmail.com>
2023-09-29 08:56:27 +0200

    validator wip

--- src/app/modules/auth/shared/password-validator.ts
+++ src/app/modules/auth/shared/password-validator.ts
@@ -1,28 +1,24 @@
 import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export function passwordValidator(control: AbstractControl): ValidationErrors | null {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
-    const valid = passwordRegex.test(control.value);
+  const value = control.value as string;
+  const errors: ValidationErrors = {};
 
-    const specialCharacterRegex = /[@$!%*?&]/;
-    const hasSpecialCharacter = specialCharacterRegex.test(control.value);
-
-    const lowercaseCharacterRegex = /[a-z]/;
-    const hasLowercaseCharacter = lowercaseCharacterRegex.test(control.value);
-
-    const errors: ValidationErrors = {};
-
-    if (!hasSpecialCharacter) {
-        errors['missingSpecialCharacter'] = true;
+  function checkRegex(regex: RegExp, errorKey: string) {
+    if (!regex.test(value)) {
+      errors[errorKey] = true;
     }
 
-    if (!hasLowercaseCharacter) {
-        errors['missingLowercaseCharacter'] = true;
-    }
+    if (value.length < 8) {
+        errors['minLength'] = true;
+      }    
+  }
 
-    if (!valid) {
-        errors['invalidPassword'] = true
-    };
+  checkRegex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&#]{8,}$/, 'invalidPassword');
+  checkRegex(/[@$!%*?&#]/, 'missingSpecialCharacter');
+  checkRegex(/[a-z]/, 'missingLowercaseCharacter');
+  checkRegex(/[A-Z]/, 'missingUppercaseCharacter');
+  checkRegex(/[\d]/, 'missingNumber');  
 
-    return Object.keys(errors).length > 0 ? errors : null;
+  return Object.keys(errors).length > 0 ? errors : null;
 }
